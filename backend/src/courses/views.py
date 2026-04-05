b552d265bf9916baba293dd83d5a468386648241
Tram <2354050145tram@ou.edu.vn>
2026-04-05 12:49:59 +0700

    feature: upate api course

--- backend/src/courses/views.py
+++ backend/src/courses/views.py
@@ -1,8 +1,12 @@
 
-from rest_framework import viewsets,generics,filters
-from courses.models import Course, Tag
+from rest_framework import viewsets,generics,filters,status
+from courses.models import Course,Tag
 from courses import serializers
 from courses.serializers import CourseSerializer, CourseDetailSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from classes.models import ClassRoom
+from classes.serializers import ClassRoomSerializer
 
 class CourseViewSet(viewsets.ViewSet,generics.ListAPIView,generics.CreateAPIView,generics.RetrieveAPIView):
       queryset = Course.objects.filter(active=True)
@@ -10,6 +14,11 @@ class CourseViewSet(viewsets.ViewSet,generics.ListAPIView,generics.CreateAPIView
         if self.action == 'retrieve':
             return serializers.CourseDetailSerializer
         return serializers.CourseSerializer
+      
+      @action(methods=['get'], url_path='classes', detail=True)
+      def get_classes(self, request, pk):
+          classes = ClassRoom.objects.select_related('course').filter(course=self.get_object(),active=True)
+          return Response(ClassRoomSerializer(classes,many=True).data,status=status.HTTP_200_OK) 
 class TagViewSet(viewsets.ViewSet,generics.CreateAPIView):
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
--- backend/src/enrollments/views.py
+++ backend/src/enrollments/views.py
@@ -3,10 +3,16 @@ from rest_framework import viewsets, parsers, generics, filters
 from enrollments.models import Enrollment, Payment
 from enrollments import serializers
 
-class EnrollmentViewSet(viewsets.ViewSet, generics.ListAPIView):
+class EnrollmentViewSet(viewsets.ViewSet, generics.ListAPIView,generics.RetrieveAPIView):
     queryset = Enrollment.objects.filter(active=True)
     serializer_class = serializers.EnrollmentSerializer
 
+    def list(self, request, *args, **kwargs):
+        return generics.ListAPIView.list(self, request, *args, **kwargs)
+    
+    def retrieve(self, request, *args, **kwargs):
+        return generics.RetrieveAPIView.retrieve(self, request, *args, **kwargs)
+    
 class PaymentViewSet(viewsets.ViewSet, generics.ListAPIView):
     queryset = Payment.objects.all()
     serializer_class = serializers.PaymentSerializer
