c19d9ba439fcd4de8cb4641916fce38cfc59fb1d
Tram <2354050145tram@ou.edu.vn>
2026-04-06 19:14:41 +0700

    feature: update course

--- backend/src/enrollments/views.py
+++ backend/src/enrollments/views.py
@@ -1,5 +1,7 @@
 
 from rest_framework import viewsets, parsers, generics, filters
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from enrollments.models import Enrollment, Payment
 from enrollments import serializers
 
@@ -7,11 +9,14 @@ class EnrollmentViewSet(viewsets.ViewSet, generics.ListAPIView,generics.Retrieve
     queryset = Enrollment.objects.filter(active=True)
     serializer_class = serializers.EnrollmentSerializer
 
-    def list(self, request, *args, **kwargs):
-        return generics.ListAPIView.list(self, request, *args, **kwargs)
-    
-    def retrieve(self, request, *args, **kwargs):
-        return generics.RetrieveAPIView.retrieve(self, request, *args, **kwargs)
+    def retrieve(self, request, pk=None):
+        try:
+            enrollment = Enrollment.objects.get(pk=pk, active=True)
+        except Enrollment.DoesNotExist:
+            raise NotFound("Enrollment not found")
+
+        serializer = serializers.EnrollmentSerializer(enrollment)
+        return Response(serializer.data)
     
 class PaymentViewSet(viewsets.ViewSet, generics.ListAPIView):
     queryset = Payment.objects.all()
